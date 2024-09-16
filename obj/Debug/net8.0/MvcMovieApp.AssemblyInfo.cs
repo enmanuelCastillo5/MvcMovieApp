b3806509975e8ceacfa2cd3050ab664132cdfd0d
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-09-16 11:05:35 -0400

    Addd Searching

--- obj/Debug/net8.0/MvcMovieApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd968859a1e1e285f779411392a7807319fa76f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1a0ca423b8a4d378b55c3999c752d3e24d132d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
